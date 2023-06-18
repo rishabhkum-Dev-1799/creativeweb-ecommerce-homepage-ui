f35e9a0603ac88314ef3be11360a48837784fbd0
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-06-18 19:30:37 +0530

    "updated - homepage component with new created featured homepage components"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navbar from '../components/header/navbar/Navbar';
+import Categories from '../components/homepage/categories/Categories';
+import NewsLetter from '../components/homepage/newsletter/NewsLetter';
+import Products from '../components/homepage/product-list/Products';
+import RotatingBanner from '../components/homepage/rotating-banner/RotatingBanner';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <RotatingBanner />
+      <Categories />
+      <Products />
+      <NewsLetter />
     </div>
   );
 };
